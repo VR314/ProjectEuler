1759d6a819dc2aa445a9bd47813ae439187d4007
VR314 <raghupathivaroon@gmail.com>
2020-06-02 20:40:06 -0400

    06/02/20 - Py 015 - Py 017

--- Python/015/015.py
+++ Python/015/015.py
@@ -1,46 +1,38 @@
-import random
+import math
 
-GRID_SIZE = 21  # 1 greater than y x y
-grid = [[j*GRID_SIZE + i for i in range(0, GRID_SIZE)]
-        for j in range(0, GRID_SIZE)]
-# print(grid)
+"""
+*	Problem #: 015
+*	Date Attempted: 05/31/20
+*	Date Solved: 06/02/20
+*
+*	How many such routes are there through a 20×20 grid?
+*
+*	Answer: 137846528820
+"""
 
-endPoint = GRID_SIZE**2 - 1
-
-
-def traverse(curr):
-    if curr != 0:
-        if int(curr / GRID_SIZE) == (GRID_SIZE - 1):  # bottom edge
-            return curr+1
-        elif curr % (GRID_SIZE) == GRID_SIZE - 1:  # right edge
-            return curr + GRID_SIZE
-
-    if random.randint(0, 1) == 0:  # move right
-        return curr+1
-    else:  # move down
-        return curr + GRID_SIZE
+GRID_SIZE = 21
+d = {GRID_SIZE**2 - 1: 1}
 
 
-path = []
-already = []
-empty = 0
-iterations = 0
-while empty < 100:
-    curr = 0
-    iterations = iterations + 1
+def findAns():
+    col = GRID_SIZE - 1
+    while col > -1:
+        for i in range(GRID_SIZE - 1, -1, -1):
+            coord = col * GRID_SIZE + i
+            d[coord] = calc(coord)
+        col = col - 1
+    return d[0]
 
-    while curr < endPoint:
-        curr = traverse(curr)
-        path.append(curr)
 
-    if(path not in already):
-        already.append(path)
-        empty = 0
+def calc(num):
+    if num == GRID_SIZE**2 - 1:
+        return 1
+    if (num + 1) % GRID_SIZE == 0 or math.floor(num / GRID_SIZE) == GRID_SIZE - 1:
+        return 1
     else:
-        empty = empty + 1
+        down = d[num + GRID_SIZE]
+        right = d[num + 1]
+        return down + right
 
-    path = []
-    if iterations % 1000 == 0:
-        print(str(iterations) + " it, " + str(len(already)))
 
-print(len(already))
+print(findAns())
---
+++ Python/017/017.py
@@ -0,0 +1,73 @@
+
+"""
+*	Problem #: 017
+*	Date Attempted: 06/02/20
+*	Date Solved: 06/02/20
+*
+*	If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+*
+*	Answer: 21124 
+"""
+
+d = {
+    0: '',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine',
+    10: 'ten',
+    11: 'eleven',
+    12: 'twelve',
+    13: 'thirteen',
+    14: 'fourteen',
+    15: 'fifteen',
+    16: 'sixteen',
+    17: 'seventeen',
+    18: 'eighteen',
+    19: 'nineteen',
+    20: 'twenty',
+    30: 'thirty',
+    40: 'forty',
+    50: 'fifty',
+    60: 'sixty',
+    70: 'seventy',
+    80: 'eighty',
+    90: 'ninety',
+    100: 'hundred',
+    1000: 'thousand'
+}
+
+
+def wordLength(x):
+    s = ''
+    if x == 1000:
+        s = s + d[int(x/1000)] + " " + d[1000]
+        s = s.replace(' ', '')
+        print(s)
+        return len(s)
+    if x >= 100:
+        s = s + d[int(x/100)] + " " + d[100]
+
+    if x % 100 != 0:
+        if x > 100:
+            s = s + " and "
+        if x % 100 > 20:
+            s = s + d[x % 100 - x % 10] + d[x % 10]
+        else:
+            s = s + d[x % 100]
+
+    s = s.replace(' ', '')
+    print(s + ": " + str(len(s)))
+    return len(s)
+
+
+sum = 0
+for i in range(1, 1001):
+    sum = sum + wordLength(i)
+
+print(sum)
